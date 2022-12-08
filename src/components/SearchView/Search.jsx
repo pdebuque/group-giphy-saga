78de27b4a7cb6b1384bc0d888c668aec82b97281
Victor Llapa <victorllapa@gmail.com>
2022-12-08 10:13:53 -0600

    added the handle for the input field

--- src/components/SearchView/Search.jsx
+++ src/components/SearchView/Search.jsx
@@ -1,12 +1,36 @@
-import 
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
+export default function SearchView() {
+	const dispatch = useDispatch();
+	const [viewSearch, setSearch] = useState('');
 
+	const handleChange = e => {
+		e.preventDefault();
+		console.log('In search input field');
+		setSearch(e.target.value);
+	};
 
+	const fetchNewGif = () => {
+		console.log('Clicked on Search button in Search.jsx');
+		dispatch({
+			type: '',
+		});
+	};
 
+	return (
+		<div>
+			<input
+				type='text'
+				name='name'
+				value={viewSearch}
+				placeholder='Search'
+				required
+				onChange={handleChange}>
+				{' '}
+			</input>
 
-export default SearchView() {
-
-
-
-
+			<button onClick={fetchNewGif}>Search</button>
+		</div>
+	);
 }
