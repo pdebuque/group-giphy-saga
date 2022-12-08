9a8bd221f1fd200e4a7c0ba5559795cfc0978540
Paolo Debuque <pdebuque@gmail.com>
2022-12-08 10:43:47 -0600

    favorite routes complete

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -5,22 +5,39 @@ const router = express.Router();
 
 // return all favorite images
 router.get('/', (req, res) => {
-    res.sendStatus(200);
+    const queryText = 'SELECT * FROM favorites';
+    pool.query(queryText)
+        .then(response => res.send(response.rows))
+        .catch(err => console.log('could not get favorites!', err))
 });
 
 // add a new favorite
 router.post('/', (req, res) => {
-    res.sendStatus(200);
+    const queryText = `INSERT INTO favorites (image_url)
+                        VALUES ($1, $2)`
+    pool.query(queryText, [req.body.image_url])
+        .then(() => res.sendStatus(200))
+        .catch(err => console.log('could not post', err))
 });
 
 // update given favorite with a category id
-router.put('/:favId', (req, res) => {
+router.put('/:id', (req, res) => {
+    const queryText = `UPDATE favorites SET categor_id=$1
+                        WHERE id = $2`
+    pool.query(queryText, [req.body.category_id, req.params.id])
+        .then(() => res.sendStatus(201))
+        .catch(err => console.log('could not update category', err))
+
     // req.body should contain a category_id to add to this favorite image
-    res.sendStatus(200);
 });
 
 // delete a favorite
-router.delete('/:favId', (req, res) => {
+router.delete('/:id', (req, res) => {
+    const queryText = `DELETE FROM favorites
+                        WHERE id = $1`
+    pool.query(queryText, [req.params.id])
+        .then(() => res.sendStatus(202))
+        .catch(err => console.log('could not delete favorite', err))
     res.sendStatus(200);
 });
 
