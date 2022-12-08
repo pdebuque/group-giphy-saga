2e9cb575b09f9760c168c4cf3b6614e581a7accc
Victor Llapa <victorllapa@gmail.com>
2022-12-08 10:57:46 -0600

    fixed routing errors

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 require('dotenv').config();
 const axios = require('axios');
 
-router.get('api/search', (req, res) => {
+router.get('/', (req, res) => {
 	axios
 		.get(
 			`http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${keyword}&limit=5`
