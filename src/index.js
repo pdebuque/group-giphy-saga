5761ff26a9707c01a0ac5af7d5221569f9a14640
Paolo Debuque <pdebuque@gmail.com>
2022-12-08 09:54:38 -0600

    set up redux in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//redux stuff
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+
+//redux-saga
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+
+const favoritesList = (state = [], action) => {
+    if (action.type === 'SET_FAVORITES') return action.payload
+    return state;
+}
+
+function* rootSaga() {
+    yield takeEvery('FETCH_FAVORITES', fetchFavorites)
+
+}
+
+function* fetchFavorites(action) {
+    const favorites = yield axios.get('/favorite');
+    yield put({ type: 'SET_FAVORITES', payload: favorites.data })
+}
+
+const sagaMiddleware = createSagaMiddleware();
+sagaMiddleware.run(rootSaga);
+
+const store = createStore(combineReducers({
+    favoritesList
+}), applyMiddleware(logger, sagaMiddleware));
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
