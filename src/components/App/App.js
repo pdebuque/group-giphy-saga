3cbd9bc13a113669a2eccfdf98bb006fa98512e1
Brett R. Gebbie <brg078@gmail.com>
2022-12-08 10:14:50 -0600

    skeleton done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import FavoriteList from '../FavoriteList/FavoriteList';
 
 function App(props) {
     return (
         <div>
             <h1>Giphy Search!</h1>
+            
+            
+            
+            
+            
+            <FavoriteList />
+
+
+
+
+
         </div>
     );
 }
